f575103b66c6386ca4555e3bb79622bb1e00d867
Travis McMahon <azgoalie@gmail.com>
2024-02-23 14:33:01 -0700

    Changed createBrowserRouter to createHashRouter for github pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { doc, getDoc } from "firebase/firestore";
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createHashRouter, Navigate } from "react-router-dom";
 
 import { db } from "./firebase";
 import HomePage from "./pages/HomePage.tsx";
@@ -7,32 +7,29 @@ import RoomPage from "./pages/RoomPage";
 
 const notFoundResponse = new Response("Not Found", { status: 404 });
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <HomePage />,
-      errorElement: <Navigate to="/" />,
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <Navigate to="/" />,
+  },
+  {
+    path: "rooms/:roomId",
+    element: <RoomPage />,
+    errorElement: <Navigate to="/" />,
+    loader: async ({ params }) => {
+      const roomId = params.roomId;
+      if (!roomId) {
+        throw notFoundResponse;
+      }
+
+      const roomRef = await getDoc(doc(db, "rooms", roomId));
+
+      if (!roomRef.exists()) {
+        throw notFoundResponse;
+      }
+
+      return roomRef;
     },
-    {
-      path: "rooms/:roomId",
-      element: <RoomPage />,
-      errorElement: <Navigate to="/" />,
-      loader: async ({ params }) => {
-        const roomId = params.roomId;
-        if (!roomId) {
-          throw notFoundResponse;
-        }
-
-        const roomRef = await getDoc(doc(db, "rooms", roomId));
-
-        if (!roomRef.exists()) {
-          throw notFoundResponse;
-        }
-
-        return roomRef;
-      },
-    },
-  ],
-  { basename: import.meta.env.BASE_URL },
-);
+  },
+]);
