a541b91d033c5f41ad22424a29e1a1aeece8ccba
Travis McMahon <azgoalie@gmail.com>
2024-02-23 14:21:38 -0700

    Fixed routing for github pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,29 +7,32 @@ import RoomPage from "./pages/RoomPage";
 
 const notFoundResponse = new Response("Not Found", { status: 404 });
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-    errorElement: <Navigate to="/" />,
-  },
-  {
-    path: "rooms/:roomId",
-    element: <RoomPage />,
-    errorElement: <Navigate to="/" />,
-    loader: async ({ params }) => {
-      const roomId = params.roomId;
-      if (!roomId) {
-        throw notFoundResponse;
-      }
-
-      const roomRef = await getDoc(doc(db, "rooms", roomId));
-
-      if (!roomRef.exists()) {
-        throw notFoundResponse;
-      }
-
-      return roomRef;
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <HomePage />,
+      errorElement: <Navigate to="/" />,
     },
-  },
-]);
+    {
+      path: "rooms/:roomId",
+      element: <RoomPage />,
+      errorElement: <Navigate to="/" />,
+      loader: async ({ params }) => {
+        const roomId = params.roomId;
+        if (!roomId) {
+          throw notFoundResponse;
+        }
+
+        const roomRef = await getDoc(doc(db, "rooms", roomId));
+
+        if (!roomRef.exists()) {
+          throw notFoundResponse;
+        }
+
+        return roomRef;
+      },
+    },
+  ],
+  { basename: import.meta.env.BASE_URL },
+);
