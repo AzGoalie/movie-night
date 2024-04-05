24fd60c995ab3b32a5d9f1d0d3027407f87f9a92
Travis McMahon <azgoalie@gmail.com>
2024-04-04 21:23:07 -0700

    Refactored create-room button to initialize firebase on click and have a spinner

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -6,19 +6,25 @@
 (def create-room-button (.getElementById js/document "create-room-button"))
 (def join-room-button (.getElementById js/document "join-room-button"))
 
-(defn- handle-create-room! []
+(defn- set-loading [element loading & {:keys [message]}]
+  (.setAttribute element "aria-busy" loading)
+  (.setAttribute element "aria-label" message))
+
+(defn- navigate-to-new-room  []
   (-> (rooms/create-room! @user)
       (.then #(navigate "/watch" {:room-id %}))))
+
+(defn- handle-create-room! []
+  (set-loading create-room-button true :message "Creating room...")
+  (-> (initialize-firebase navigate-to-new-room)
+      (.catch #(set-loading create-room-button false))))
 
 (defn- handle-join-room! []
   (println "Joining room..."))
 
-(defn setup-buttons []
+(defn ^:dev/after-load init []
   (.addEventListener create-room-button "click" handle-create-room!)
   (.addEventListener join-room-button "click" handle-join-room!))
-
-(defn ^:dev/after-load init []
-  (initialize-firebase setup-buttons))
 
 (defn ^:dev/before-load stop
   "Used in development to reset state for hot-reloading"
