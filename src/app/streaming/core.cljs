9ab3445ab246907d825bded09ab6a1292e73bd41
Travis McMahon <azgoalie@gmail.com>
2024-04-05 10:50:51 -0700

    Added loading message when joining a room

--- src/app/streaming/core.cljs
+++ src/app/streaming/core.cljs
@@ -6,11 +6,19 @@
             [app.streaming.video :as video]
             [app.utils :refer [get-query-params navigate  set-hidden!]]))
 
+(defn- show-room []
+  (let [loading-section (.getElementById js/document "loading")
+        room-wrapper (.getElementById js/document "room-wrapper")]
+    (set-hidden! loading-section true)
+    (set-hidden! room-wrapper false)))
+
 (defn- setup-room
   "Attach event listeners to video player and setup owner or viewer."
   [{:keys [owner id]}]
   (let [video-player   (.getElementById js/document "video-player")
         status-section (.getElementById js/document "status-section")]
+
+    (show-room)
 
     (video/add-on-playing! video-player #(set-hidden! status-section true))
     (video/add-on-ended! video-player #(set-hidden! status-section false))
